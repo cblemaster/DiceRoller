3fb35741ca1de4f742a387b467234d57ff712989
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-25 12:51:25 -0500

    added dice roll class with dir props and roll method

---
+++ DiceRoller.MAUI/DiceRoll.cs
@@ -0,0 +1,18 @@
+﻿namespace DiceRoller.MAUI
+{
+    public class DiceRoll
+    {
+        private readonly uint[] _validSides = {2, 3, 4, 6, 8, 10, 12, 20, 100 };
+        private readonly Random _random = new();
+                
+        public required uint Sides { get; set; }
+        public required uint Count { get; set; }
+        public required int Modifier { get; set; }
+
+        public bool IsSidesValid => _validSides.Contains<uint>(Sides);
+        public bool IsModifierValid => Modifier is <= 100 and >= (-100);
+        public bool IsCountValid => Count > 0;
+
+        public int Roll() => throw new NotImplementedException();
+    }
+}
