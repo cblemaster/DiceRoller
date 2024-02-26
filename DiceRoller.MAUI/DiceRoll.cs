62928ae095b4298bcbca810130234dbe4d923f37
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-25 22:55:19 -0500

    substantial changes to the dice roller class as te ui was developed

--- DiceRoller.MAUI/DiceRoll.cs
+++ DiceRoller.MAUI/DiceRoll.cs
@@ -2,39 +2,49 @@
 {
     public class DiceRoll
     {
-        private readonly uint[] _validSides = [2, 3, 4, 6, 8, 10, 12, 20, 100];
         private readonly Random _random = new();
-                
-        public required uint Sides { get; set; }
-        public required uint Count { get; set; }
-        public required int Modifier { get; set; }
+        public static IEnumerable<uint> ValidSides => [2, 3, 4, 6, 8, 10, 12, 20, 100];
 
-        public bool IsSidesValid => _validSides.Contains<uint>(Sides);
+        public required uint Sides { get; init; }
+        public required uint Count { get; init; }
+        public required int Modifier { get; init; }
+
+        public bool IsSidesValid => ValidSides.Contains<uint>(Sides);
+        public bool IsCountValid => (Sides == 2 || Sides == 3 || Sides == 100) && Count == 1 || Count > 0; // d2 and d3 can only be rolled with count of one (1); it also doesn't make sense to roll more than one (1) d100
+
         public bool IsModifierValid => Modifier is <= 100 and >= (-100);
-        public bool IsCountValid => Count > 0 && ((Sides == 2 || Sides == 3) && Count == 1);    // d2 and d3 can only be rolled with count of one (1)
 
         public IEnumerable<uint> Roll()
         {
             /* I consulted the 5th edition player's handbook
                for guidance on dice rolling rules */
-            
-            if (Sides == 100) { return [RollPercentile()]; }    // d100 has special rules
 
             List<uint> dieRolls = [];
 
-            if (!IsSidesValid || ! IsModifierValid || !IsCountValid) { return dieRolls; }
+            if (!IsSidesValid || !IsModifierValid || !IsCountValid) { return dieRolls; }
+
+            if (Sides == 100)   // d100 has special rules
+            {
+                dieRolls.Add(RollPercentile());
+                return dieRolls;
+            }
+
+            //TODO: d2 and d3
+            if (Sides == 2) { }
+            if (Sides == 3) { }
 
             // generate random for each Count and add to collection to return
             for (int i = 1; i <= Count; i++)
             {
                 dieRolls.Add(GetRandom(Sides));
             }
-            
-            return dieRolls;
+
+            return dieRolls.AsEnumerable<uint>();
         }
+
         public uint RollPercentile()
         {
-            // per the 5th edition PH d100 is two (2) d10s
+            // Per the 5th edition PH d100 is two (2) d10s
             // the first representing the tens place
             // the second representing the ones place
 
@@ -47,9 +57,9 @@
             }
 
             string combined = tens.ToString() + ones;
-            return uint.TryParse(combined, out uint result)
-                ? result
-                : throw new InvalidCastException("An error occured calculating the percentile dice result.");
+            _ = uint.TryParse(combined, out uint percentile);
+
+            return percentile;
         }
 
         public uint GetRandom(uint sides = 20) => (uint)(_random.Next((int)sides + 1));
