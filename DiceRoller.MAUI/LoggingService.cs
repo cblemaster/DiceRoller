8ceadc4e5237f229c65c2449370168dbbc43cd67
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-26 20:59:02 -0500

    added logging service class

---
+++ DiceRoller.MAUI/LoggingService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace DiceRoller.MAUI
+{
+    public class LoggingService
+    {
+        private const string FILE = "DiceRollsLog.txt";
+        private const string PATH = @"..\..\..\..\Log";
+        
+        public void WriteRollResultToLog(string selected, string rolls, string modifier, string total)
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("******************************");
+            sb.AppendLine($"Dice Roll: {DateTime.Now.ToLongDateString()}");
+            sb.AppendLine($"Selected dice: {selected}");
+            sb.AppendLine($"Individual rolls: {rolls}");
+            sb.AppendLine($"Modifier: {modifier}");
+            sb.AppendLine($"TOTAL Result: {total}");
+            sb.AppendLine("******************************");
+
+            string directory = Environment.CurrentDirectory;
+            string fullpath = Path.Combine(directory, PATH, FILE);
+
+            try
+            {
+                using StreamWriter sw = new(fullpath, false);
+                sw.WriteLine(sb);
+            }
+            catch (IOException) { throw; }
+        }
+    }
+}
