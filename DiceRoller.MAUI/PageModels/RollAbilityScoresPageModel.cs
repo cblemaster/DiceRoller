e2b1eec98eddfef4cb59e6570d303824f4078071
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-28 09:01:46 -0500

    completed roll ability scores page model

--- DiceRoller.MAUI/PageModels/RollAbilityScoresPageModel.cs
+++ DiceRoller.MAUI/PageModels/RollAbilityScoresPageModel.cs
@@ -1,12 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using DiceRoller.MAUI.Models;
 using DiceRoller.MAUI.Pages;
+using System.Collections.ObjectModel;
 
-namespace DiceRoller.MAUI.PageModels
+namespace DiceRoller.MAUI.PageModels;
+
+public partial class RollAbilityScoresPageModel : ObservableObject
 {
-    public partial class RollAbilityScoresPageModel : ObservableObject
+    [ObservableProperty]
+    private ReadOnlyCollection<AbilityScoreResult> _rollResults = default!;
+        
+    [RelayCommand]
+    private void HowItWorksClicked() => Shell.Current.Navigation.PushModalAsync(new HowItWorksPage());
+    
+    [RelayCommand]
+    private void RollAbilityScoresClicked()
     {
-        [RelayCommand]
-        private void HowItWorksClicked() => Shell.Current.Navigation.PushModalAsync(new HowItWorksPage());
+        DiceRoll roller = new() { Sides = 6, Count = 4, Modifier = 4 };
+        RollResults = roller.RollAbiltyScores().OrderBy(r => r.RollTotal).ToList().AsReadOnly();
+
+        // TODO: Logging ... !!
     }
 }
