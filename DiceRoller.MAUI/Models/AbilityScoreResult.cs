2fbc8c19968c95e653b2076dad931f93fe6f9908
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-28 09:00:25 -0500

    added ability score result model

---
+++ DiceRoller.MAUI/Models/AbilityScoreResult.cs
@@ -0,0 +1,10 @@
+﻿namespace DiceRoller.MAUI.Models;
+
+public class AbilityScoreResult
+{
+    public required string DiscardedRoll { get; init; }
+
+    public required string Rolls { get; init; }
+
+    public required string RollTotal { get; init; }
+}
